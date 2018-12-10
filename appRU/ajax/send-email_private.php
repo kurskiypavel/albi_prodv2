75632d025383a366a23304d40c6dbfe283693b61
Aegis <paul@aegisbrand.com>
2018-12-10 14:13:04 -0500

    some dump update

--- appRU/ajax/send-email_private.php
+++ appRU/ajax/send-email_private.php
@@ -8,50 +8,45 @@ $emailFrom = 'kurskiy.ifc@gmail.com';
 $emailTo = 'Albina.kurskaya@gmail.com';
 
 
-
-
-if(!empty($date)){
-    $curl = curl_init();
-}
-
+$curl = curl_init();
 
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => '{
+    CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => '{
     "personalizations":[
        {
           "to":[
              {
-                "email":"'.$emailTo.'",
+                "email":"' . $emailTo . '",
                 "name":"Albina Kurskaya"
              }
           ],
           "dynamic_template_data":{
-             "date":"'.$date.'",
-             "time":"'.$time.'",
-             "user":"'.$user.'",
-             "comment":"'.$comment.'",
-             "repeatable":"'.$repeatable.'"
+             "date":"' . $date . '",
+             "time":"' . $time . '",
+             "user":"' . $user . '",
+             "comment":"' . $comment . '",
+             "repeatable":"' . $repeatable . '"
           }
        }
     ],
     "from":{
-       "email":"'.$emailFrom.'",
+       "email":"' . $emailFrom . '",
        "name":"Albi Yoga"
     },
-    "template_id":"'.$template_id.'"
+    "template_id":"' . $template_id . '"
  }',
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Bearer ".$authorization."",
-    "content-type: application/json"
-  ),
+    CURLOPT_HTTPHEADER => array(
+        "authorization: Bearer " . $authorization . "",
+        "content-type: application/json"
+    ),
 ));
 
 $response = curl_exec($curl);
@@ -59,11 +54,11 @@ $err = curl_error($curl);
 
 curl_close($curl);
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
+//if ($err) {
+//    echo "cURL Error #:" . $err;
+//} else {
+//    echo $response;
+//}
 
 
 /*
--- appRU/config.php
+++ appRU/config.php
@@ -2,31 +2,31 @@
 
 
 //PRODUCTION
- $host = null;
- $user = "root";
- $password = "c484b8572c29633e048871c2ee39144cc161835e9041f502";
- $database = "albiRU";
- $port = null;
-
- $conn = new mysqli($host,$user,$password, $database,$port);
- $conn->set_charset("utf8");
- if(mysqli_connect_error()){
-     echo mysqli_connect_error();
-     exit();
- }
+// $host = null;
+// $user = "root";
+// $password = "c484b8572c29633e048871c2ee39144cc161835e9041f502";
+// $database = "albiRU";
+// $port = null;
+//
+// $conn = new mysqli($host,$user,$password, $database,$port);
+// $conn->set_charset("utf8");
+// if(mysqli_connect_error()){
+//     echo mysqli_connect_error();
+//     exit();
+// }
 
 //LOCALHOST
-//$host = null;
-//$user = "root";
-//$password = "root";
-//$database = "playground";
-//$port = null;
-//$socket = "/Applications/MAMP/tmp/mysql/mysql.sock";
-//$conn = new mysqli($host, $user, $password, $database, $port, $socket);
-//if (mysqli_connect_error()) {
-//    echo mysqli_connect_error();
-//    exit();
-//};
+$host = null;
+$user = "root";
+$password = "root";
+$database = "playground";
+$port = null;
+$socket = "/Applications/MAMP/tmp/mysql/mysql.sock";
+$conn = new mysqli($host, $user, $password, $database, $port, $socket);
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit();
+};
 
 
 ?>
